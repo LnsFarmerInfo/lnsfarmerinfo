7fc3684224c81ced792a4260fb1e14049c1c22db
Vinayak Nawdhar <vinayaknawdhar003@gmail.com>
2024-12-30 20:49:45 +0530

    Update page.js

--- app/internship/page.js
+++ app/internship/page.js
@@ -1,19 +1,20 @@
-// import React from 'react'
-// import Internship from '../Components/Internship'
-// import User from '@/models/User'
-// import { connectDB } from '@/lib/mongodb'
+import React from 'react'
+import Internship from '../Components/Internship'
+import User from '@/models/User'
+import { connectDB } from '@/lib/mongodb'
 
-// await connectDB();
-// async function getCount(){
-//   const resp = await User.find()
-//   console.log(resp.length)
-//   return resp.length
-// }
-// const page = async () => {
-//   const count = await getCount()
-//   return (
-//     <Internship count={count}/>
-//   )
-// }
+await connectDB();
+async function getCount(){
+  const resp = await User.find()
+  console.log(resp.length)
+  return resp.length
+}
+const page = async () => {
+  const count = await getCount()
+  return (
+    // <Internship count={count}/>
+    <h1>PAGE NOT FOUND!</h1>
+  )
+}
 
-// export default page
+export default page
